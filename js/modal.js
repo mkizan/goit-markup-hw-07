9408baf44e58931f70dda867fc4b64b69ccea374
Mykola Kizan <mkizan@gmail.com>
2021-01-09 20:47:02 +0200

    renamed classes and wrote code on scss for index.html page

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,16 @@
 const refs = {
-    openModalBtn: document.querySelector("[data-open-modal]"),
-    closeModalBtn: document.querySelector("[data-close-modal]"),
-    backdrop: document.querySelector("[data-backdrop]"),
+	openModalBtn: document.querySelector('[data-open-modal]'),
+	closeModalBtn: document.querySelector('[data-close-modal]'),
+	backdrop: document.querySelector('[data-backdrop]'),
 };
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
-refs.backdrop.addEventListener("click", logBackdropClick);
+refs.backdrop.addEventListener('click', logBackdropClick);
 
 function toggleModal() {
-    refs.backdrop.classList.toggle("is-hidden");
+	refs.backdrop.classList.toggle('is-hidden');
 }
 
-function logBackdropClick() {
-    
-}
+function logBackdropClick() {}
